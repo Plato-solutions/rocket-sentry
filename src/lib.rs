941d59c3f9b59215271f4e4541902ef236023151
Ryan Miller <7181449-platosolutions@users.noreply.gitlab.com>
2022-09-19 14:17:30 -0400

    make this work for Plato's outdated hacked libraries

--- src/lib.rs
+++ src/lib.rs
@@ -45,7 +45,8 @@ use std::sync::Mutex;
 use rocket::fairing::{Fairing, Info, Kind};
 use rocket::serde::Deserialize;
 use rocket::{fairing, Build, Rocket};
-use sentry::ClientInitGuard;
+use logging::ClientInitGuard;
+use logging::sentry::{Span, TransactionOrSpan};
 
 pub struct RocketSentry {
     guard: Mutex<Option<ClientInitGuard>>,
@@ -57,25 +58,25 @@ struct Config {
 }
 
 impl RocketSentry {
-    pub fn fairing() -> impl Fairing {
+    pub fn fairing(sentry:ClientInitGuard) -> impl Fairing {
         RocketSentry {
-            guard: Mutex::new(None),
+            guard: Mutex::new(Some(sentry)),
         }
     }
 
-    fn init(&self, dsn: &str) {
-        let guard = sentry::init(dsn);
-
-        if guard.is_enabled() {
-            // Tuck the ClientInitGuard in the fairing, so it lives as long as the server.
-            let mut self_guard = self.guard.lock().unwrap();
-            *self_guard = Some(guard);
-
-            info!("Sentry enabled.");
-        } else {
-            error!("Sentry did not initialize.");
-        }
-    }
+    // fn init(&self) {
+    //     let guard = logging::init();
+    //
+    //     if guard.is_enabled() {
+    //         // Tuck the ClientInitGuard in the fairing, so it lives as long as the server.
+    //         let mut self_guard = self.guard.lock().unwrap();
+    //         *self_guard = Some(guard);
+    //
+    //         info!("Sentry enabled.");
+    //     } else {
+    //         error!("Sentry did not initialize.");
+    //     }
+    // }
 }
 
 #[rocket::async_trait]
@@ -86,21 +87,38 @@ impl Fairing for RocketSentry {
             kind: Kind::Ignite | Kind::Singleton,
         }
     }
+    //
+    // async fn on_ignite(&self, rocket: Rocket<Build>) -> fairing::Result {
+    //     let figment = rocket.figment();
+    //
+    //     let config: figment::error::Result<Config> = figment.extract();
+    //     match config {
+    //         Ok(config) => {
+    //             if config.sentry_dsn.is_empty() {
+    //                 info!("Sentry disabled.");
+    //             } else {
+    //                 self.init(&config.sentry_dsn);
+    //             }
+    //         }
+    //         Err(err) => error!("Sentry not configured: {}", err),
+    //     }
+    //     Ok(rocket)
+    // }
 
-    async fn on_ignite(&self, rocket: Rocket<Build>) -> fairing::Result {
-        let figment = rocket.figment();
+    async fn on_request(&self, req: &mut rocket::Request<'_>, data: &mut rocket::Data<'_>) {
+        let mut headers = vec![];
+        if let Some(sentry_trace) = req.headers().get_one("sentry-trace") {
+            headers.push(("sentry-trace",sentry_trace)); }
 
-        let config: figment::error::Result<Config> = figment.extract();
-        match config {
-            Ok(config) => {
-                if config.sentry_dsn.is_empty() {
-                    info!("Sentry disabled.");
-                } else {
-                    self.init(&config.sentry_dsn);
-                }
-            }
-            Err(err) => error!("Sentry not configured: {}", err),
-        }
-        Ok(rocket)
+        let transaction_ctx = logging::sentry::TransactionContext::continue_from_headers(
+            "ApiGateway",
+            "on_request",
+            headers,
+        );
+        let transaction =  logging::sentry::start_transaction(transaction_ctx);
+        logging::sentry::configure_scope(|scope| scope.set_span(Some( transaction.clone().into())));
+
+        //add data to scope?
+        //force to beginning of scope?
     }
 }
